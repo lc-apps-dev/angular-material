0ee6b3811f0c6fdc2756897a5c25e945fb139c3d
lc-apps-dev <lc-apps-dev>
2021-10-29 14:44:59 +0200

    navigation splitted into components

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    WelcomeComponent,  
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,  
 
   ],
   imports: [
